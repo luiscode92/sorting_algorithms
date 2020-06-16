aa87cf16e3465aa405b06444c6abf6dcb5eefa3e
Luis Enrique Sanjuan Melo <luiscode92@gmail.com>
2020-06-16 20:43:29 +0000

    update

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -8,26 +8,24 @@
 
 void bubble_sort(int * array, size_t size)
 {
-    unsigned int i
-    int swap, tmp
+    unsigned int i;
+    int swap, tmp;
 
     if (!array)
-    return
+    return;
 
     do {
-        swap = 0
-        for (i=0
-             i < size - 1
-             i + +)
+        swap = 0;
+        for (i=0 ; i < size - 1 ; i++)
         {
             if (array[i] > array[i + 1])
             {
-                tmp = array[i + 1]
-                array[i + 1] = array[i]
-                array[i] = tmp
-                swap = 1
-                print_array(array, size)
+                tmp = array[i + 1];
+                array[i + 1] = array[i];
+                array[i] = tmp;
+                swap = 1;
+                print_array(array, size);
             }
         }
-    } while (swap)
+    } while (swap);
 }
