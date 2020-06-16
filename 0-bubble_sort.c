d5a243003c50319dc325a40145067ab81f6e378d
Luis Enrique Sanjuan Melo <luiscode92@gmail.com>
2020-06-16 21:01:17 +0000

    betty fix

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,31 +1,29 @@
-# include "sort.h"
+#include "sort.h"
 
 /**
 * bubble_sort - sort integrers using th bubble sort alg
-* @ array: the array of integers
-* @ size: the number of elements
+* @array: the array of integers
+* @size: the number of elements
 */
-
-void bubble_sort(int * array, size_t size)
+void bubble_sort(int *array, size_t size)
 {
-    unsigned int i;
-    int swap, tmp;
+	unsigned int i;
+	int swap, tmp;
 
-    if (!array)
-    return;
-
-    do {
-        swap = 0;
-        for (i=0 ; i < size - 1 ; i++)
-        {
-            if (array[i] > array[i + 1])
-            {
-                tmp = array[i + 1];
-                array[i + 1] = array[i];
-                array[i] = tmp;
-                swap = 1;
-                print_array(array, size);
-            }
-        }
-    } while (swap);
+	if (!array)
+		return;
+	do {
+		swap = 0;
+		for (i = 0 ; i < size - 1 ; i++)
+		{
+			if (array[i] > array[i + 1])
+			{
+				tmp = array[i + 1];
+				array[i + 1] = array[i];
+				array[i] = tmp;
+				swap = 1;
+				print_array(array, size);
+			}
+		}
+	} while (swap);
 }
