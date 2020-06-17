f73af67471dc5b4fc94a75755ee31d076fd5daa8
Luis Enrique Sanjuan Melo <luiscode92@gmail.com>
2020-06-17 21:04:51 +0000

    update

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -7,33 +7,30 @@
 
 void shell_sort(int *array, size_t size)
 {
-    size_t gap = 1, i = 0, j = 0;
-    int tmp;
+	size_t gap = 1, i = 0, j = 0;
+	int tmp;
 
-    while (gap < size)
-	    gap = gap * 3 + 1;
-
-    gap = ((gap - 1) / 3);
-    
-    for (; gap > 0 ; gap = ((gap - 1) / 3))
-    {
-	    for (i = gap; i < size; i++)
-	    {
-		    tmp = array[i];
-		    for (j = i ; j >= gap && array[j - gap] > tmp; j -= gap)
-			    array[j] = array[j - gap];
-		    array[j] = tmp;
-	    }
-
-	    print_array(array, size);
-    }
+	while (gap < size)
+		gap = gap * 3 + 1;
+	gap = ((gap - 1) / 3);
+	for (; gap > 0 ; gap = ((gap - 1) / 3))
+	{
+		for (i = gap; i < size; i++)
+		{
+			tmp = array[i];
+			for (j = i ; j >= gap && array[j - gap] > tmp; j -= gap)
+				array[j] = array[j - gap];
+			array[j] = tmp;
+		}
+		print_array(array, size);
+	}
 }
 
 /**
   *swap - swaps two elements
   *@a: first element
   *@b: second elelemnt
-  *@arr: array
+  *@ar: array
   *@size: size of array
   */
 void swap(int *a, int *b, size_t size, int *ar)
